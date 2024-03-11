f77dc12c7f857a465d51965932865284a1495836
highcera <116418627+highcera@users.noreply.github.com>
2024-03-11 18:11:36 +0900

    O240311

---
+++ Exam/src/Udemic/U1_071.java
@@ -0,0 +1,11 @@
+package Udemic;
+
+public class U1_071 {
+    public static void main(String[] args) {
+        boolean bool = false;
+        while (bool) {
+            System.out.println("Hello");
+        }
+        System.out.println("Finished");
+    }
+}
---
+++ Exam/src/Udemic/U1_077.java
@@ -0,0 +1,18 @@
+package Udemic;
+
+public class U1_077 {
+    public String rideWave() { return null; }
+ 
+    private void rideWave(int value, String value2) {}
+    protected void riceWave(int a) {}
+ 
+    // public String[] rideWave() { return null; }
+    // method rideWave() is already defined
+ 
+    // default StringBuilder rideWave (StringBuffer a) { return null; }
+    // modifier default not allowed here
+ 
+    public static void main(String[] args) {
+        System.out.println("Normally Completed!");
+    }
+}
